f0c65c612d8a9ec270558da9090fd261e020cf01
Nguyen Ba Linh <linhnb@asiantech.vn>
2015-07-22 13:54:36 +0700

    Create App

---
+++ HowSay/HowSay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HowSay
+//
+//  Created by asiantech on 7/22/15.
+//  Copyright (c) 2015 asiantech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
