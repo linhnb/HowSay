c584fa97904704a44fb4116edce3a2e733bf7866
Nguyen Ba Linh <linhnb@asiantech.vn>
2015-07-23 11:55:13 +0700

    CollectionView

---
+++ HowSay/HowSay/Classes/Controller/HomeVC/HomeCell/HomeCell.swift
@@ -0,0 +1,18 @@
+//
+//  HomeCell.swift
+//  HowSay
+//
+//  Created by asiantech on 7/23/15.
+//  Copyright (c) 2015 asiantech. All rights reserved.
+//
+
+import UIKit
+
+class HomeCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ HowSay/HowSay/Classes/View/CircleImage/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  HowSay
+//
+//  Created by asiantech on 7/23/15.
+//  Copyright (c) 2015 asiantech. All rights reserved.
+//
+
+import UIKit
+
+class CircleImage: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
